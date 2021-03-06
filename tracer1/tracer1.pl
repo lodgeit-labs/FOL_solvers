f1da79e2c7c2dbfcc67b865a1dfea434175f1bd5
koo5 <kolman.jindrich@gmail.com>
2021-03-06 09:39:03 +0100

    tracer1...

--- tracer1/tracer1.pl
+++ tracer1/tracer1.pl
@@ -1,25 +1,25 @@
 /* i guess i should make this two-level:
 first level pattern-matches on the code, can use cuts, and dispatches to the second level*/
-trc(true,true).
-trc((A,B),and(ProofA,ProofB)):-
-	trc(A,ProofA),trc(B,ProofB).
-trc((A->B;C),ifthenelse(ProofA,ProofB,ProofC)):-
-	trc(A,ProofA)
+trc(_,true,true).
+trc(Module, (A,B),and(ProofA,ProofB)):-
+	trc(Module, A,ProofA),trc(Module, B,ProofB).
+trc(Module, (A->B;C),ifthenelse(ProofA,ProofB,ProofC)):-
+	trc(Module, A,ProofA)
 	->
-	trc(B,ProofB)
+	trc(Module, B,ProofB)
 	;
-	trc(C,ProofC).
-trc((A;B),or(Proof)):-
+	trc(Module, C,ProofC).
+trc(Module, (A;B),or(Proof)):-
 	A \= (_->_),
-	(trc(A,Proof);
-	trc(B,Proof)).
-trc((A->B),ifthen(ProofA,ProofB)):-
-	trc(A,ProofA)
+	(trc(Module, A,Proof);
+	trc(Module, B,Proof)).
+trc(Module, (A->B),ifthen(ProofA,ProofB)):-
+	trc(Module, A,ProofA)
 	->
-	trc(B,ProofB).
+	trc(Module, B,ProofB).
 
-trc(A, (A :- Proof)) :-
-	A =.. [call,Arg|Args],
+trc(Module, A, (A :- Proof)) :-
+	catch(A =.. [call,Arg|Args],_,fail),
 	(	atom(Arg)
 	->	C =.. [Arg|Args]
 	;	(
@@ -28,25 +28,86 @@ trc(A, (A :- Proof)) :-
 			C =.. [Fn|New_args]
 		)
 	),
-	trc(C, Proof).
+	trc(Module, C, Proof).
 
-trc(Q, (Q :- Proof)):-
-	compound_name_arity(Q, Name, _),
-	Name \= ',',
-	Name \= ';',
-	Name \= '->',
-	Name \= 'call',
+trc(Module, Q, (Q :- Proof)):-
+	(	atom(Q)
+	->	Name = Q
+	;	compound_name_arity(Q, Name, _)),
+	Name \= ',',Name \= ';',Name \= '->',Name \= 'call',
+
+	(	sub_atom(Name, 0, _, _, $)
+	->	Body = builtin(Q)
+	;	trc_clause(Module,Q,Body)),
+
+	(
+		(
+			Body = builtin(X),
+			%writeq(call(Module:X)),nl,
+			writeq(Module:X),nl,
+			(	sub_atom(Name, 0, _, _, $)
+			->	call_system_something_with_module(Module, Q)
+			;	call(Module:X)),
+			Proof=builtin
+		)
+		;
+		(
+			Body = loaded(X,Module2),
+			writeq(Module2:X),nl,
+			trc(Module2, X, Proof)
+		)
+	).
+
+call_system_something_with_module(Module, Q) :-
+	gtrace,
+	(	Q =.. [SysFn]
+	->	Q2 = Q
+	;	(
+			Q =.. [SysFn|R],
+			(	R = [Fn]
+			->	true
+			;	throw_string('uhh')),
+			Q2 =.. [SysFn,(Module:Fn)]
+		)
+	),
+	call(Q2).
+
+
+trc_clause(Module,Q,Body) :-
 	catch(
-		(clause(Q,B),Body = user(B)),
+		clause(Module:Q,B,Ref),
 		_,
 		(Body = builtin(Q))
 	),
-	(	Body = builtin(X)
-	->	(call(X),Proof=builtin)
-	;	(Body = user(X), trc(X, Proof))).
+	(	var(Body)
+	->	(
+			Body = loaded(B,Module2),
+			(
+				(
+					clause_property(Ref, module(Module2)),
+					Module2 \= system
+				)
+			->	true
+			;	Module2=Module
+			)
+		)
+	;	true).
+
+
+
 
 
 trc(X) :-
-	trc(X, GP),
-	writeq(GP).
+	trc(user, X, GP),
+	print_term(GP, [write_options([
+				numbervars(true),
+				quoted(true),
+				portray(true)])]),
+	nl.
+	%writeq(GP).
 
+
+
+/*
+debug,use_module(library(plunit)),gtrace,trc(run_tests).
+*/
