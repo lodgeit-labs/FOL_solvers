69fb4ff0547f6cb504ae96ee7f38f2505d9d040b
koo5 <kolman.jindrich@gmail.com>
2021-07-25 15:32:03 +0200

    jsonld transforms..

--- json-ld/test1/src/cars.js
+++ json-ld/test1/src/cars.js
@@ -9,19 +9,55 @@ var jl = require('jsonld');
 const { Command } = require('commander');
 const program = new Command();
 
+
+async function cars_framed(source)
+{
+	let s = await processor.load_n3(source);
+	console.debug('loaded')
+	let framed = await processor.frame(s, {
+		"@context":
+			{
+				"excel": "https://rdf.lodgeit.net.au/v1/excel#",
+				"cars": "https://rdf.lodgeit.net.au/v1/cars#",
+				"rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+
+			},
+		"@embed": "@once",
+		"@requireAll": true,
+		"excel:sheet_type": {"@id": "cars:car_sheet"},
+		"rdf:value":
+			{
+				"@list": [
+					{
+						"@embed": "@always"
+					}
+				]
+			}
+	});
+	return framed;
+}
+
 program
 	.command('jsonld <source>')
 	.description('json-ld from cars rdf')
 	.action(async (source) => {
-		let s = await processor.load_n3(source);
-		console.debug('loaded')
-		let framed = await processor.frame(s, {
-		    "@id": "http://ex.com/request",
-		    "cars:car_sheet_rows":{
-
+		console.log(JSON.stringify(await cars_framed(source), null, 2))
+});
+program
+	.command('simplified <source>')
+	.description('simplified json from cars rdf')
+	.action(async (source) => {
+		let f = await cars_framed(source);
+		let items = f['@graph'][0]['rdf:value']['@list'];
+		items.forEach(i => {
+			for (const [key, value] of Object.entries(i))
+			{
+				let v = value['rdf:value'];
+				if (v !== undefined)
+					i[key] = v;
 			}
 		});
-		console.log(framed);
+		console.log(JSON.stringify(await items, null, 2))
 });
 
 program.parse(process.argv);
