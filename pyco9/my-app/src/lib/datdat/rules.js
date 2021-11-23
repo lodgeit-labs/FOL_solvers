a5f9aa77231d2f7bf78a86127c4db7e95b83fda5
koo5 <kolman.jindrich@gmail.com>
2021-11-23 01:09:46 +0100

    wrapping up for now..

--- pyco9/my-app/src/lib/datdat/rules.js
+++ pyco9/my-app/src/lib/datdat/rules.js
@@ -1,3 +1,11 @@
+/*
+
+a b c.
+	1	2	3
+d e f.
+	11	12	13	21	22	23	31	32	33
+
+*/
 
 
 
@@ -42,4 +50,41 @@ class Rule:
 		rules with matching predicate, arity, and constants
 		"""
 	]
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+#define yield(x) {state.entry = (char*)&&x - (char*)&&case0; return state.entry;}
+
+
+struct Coro
+{
+	size_t entry;
+}
+
+class Body: public Coro
+{
+	Coro *coros;
+	Thing *locals;
+}
+
+class Term: public Coro
+{
+	vector<*Thing> vars;
+	Coro *coros;
+}
+
+
+
+
+
 
